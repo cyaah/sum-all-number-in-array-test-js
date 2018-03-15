8263e073c9f7b897871b8113a34a62e32451b0a1
Calvin Yah <calvin.yah@gmail.com>
2018-03-15 22:57:42 +0000

    Solved sum array problem

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+  array.forEach((item) => {
 
+    if (Array.isArray(item)) {
+     sum += sumItems(item);
+    } else {
+      sum  += item;
+  
+    }
+  });
+  return sum;
+}
+let array =[[1, 2, [[3], 4]], 5, []];
+console.log(sumItems(array));
 module.exports = sumItems;
